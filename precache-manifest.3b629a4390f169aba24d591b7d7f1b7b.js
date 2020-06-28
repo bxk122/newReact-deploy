75f221013c3082971d2ee9e5997fca2fa3105bd1
Badal Karki <badalkarki2@gmail.com>
2020-06-28 13:56:35 -0500

    Updates

---
+++ precache-manifest.3b629a4390f169aba24d591b7d7f1b7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "876f0ec23c78cfa1657dd100cb6c4c8e",
+    "url": "/newReact-deploy/index.html"
+  },
+  {
+    "revision": "6614df7650f14e4dbecb",
+    "url": "/newReact-deploy/static/css/main.f6117fe3.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/newReact-deploy/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/newReact-deploy/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6614df7650f14e4dbecb",
+    "url": "/newReact-deploy/static/js/main.994d4c73.chunk.js"
+  },
+  {
+    "revision": "34bff87e39926a8be773",
+    "url": "/newReact-deploy/static/js/runtime-main.897597be.js"
+  },
+  {
+    "revision": "9f5c6c4634a9ab3d17a3c800e6f3075f",
+    "url": "/newReact-deploy/static/media/badal2.9f5c6c46.png"
+  }
+]);
