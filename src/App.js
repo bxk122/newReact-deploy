44930ed24ccc8c29f95feee164c42c8714175e8e
Badal Karki <badalkarki2@gmail.com>
2020-06-28 13:46:44 -0500

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './badal2.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <img className="photo" src={logo} alt="image" />
+        <p style={{fontSize: '2rem'}}>
+          Team Members:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ul>
+          <li>Anish Karki</li>
+          <li>Ashis Rajthala</li>
+          <li>Badal karki</li>
+          <li>Nischal Shrestha</li>
+        </ul>
     </div>
   );
 }
